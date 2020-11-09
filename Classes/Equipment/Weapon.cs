edc1cba81f4eb598bd379f70e809cbb2d462cdb6
DiegoVivas13 <62918291+DiegoVivas13@users.noreply.github.com>
2020-11-08 19:09:44 -0500

    Agregar archivos de proyecto.

---
+++ Classes/Equipment/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using FirstFantasy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstFantasy.Classes.Equipment
+{
+    public abstract class Weapon : IDescribable
+    {
+        public string ShowInformation()
+        {
+            return "This is a weapon";
+        }
+
+        public override string Attack()
+        {
+            int a = new Random().Next(0, 9);
+            string b = " ";
+            return b + a;
+        } 
+    }
+}
